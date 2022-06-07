e6c620af7479cdce55267168a32bceb9933b5a1f
vittal <vittaludi@gmail.com>
2022-06-07 14:37:50 +0530

    commit

---
+++ Strings/src/String/Main.java
@@ -0,0 +1,17 @@
+package String;
+
+public class Main {
+	
+
+		public static void main(String args[]){
+
+		String str = "Programming";
+
+		String s = new String("World");
+
+		System.out.println(str);
+
+		System.out.println(s);
+
+}
+}
---
+++ Strings/src/String/Prog1.java
@@ -0,0 +1,10 @@
+package String;
+
+public class Prog1 {
+	public static void main(String[] args) {
+		String s ="Hadrik Pandya";
+		System.out.println(s.substring(7));
+		System.out.println(s.substring(0,7));
+	}
+
+}
